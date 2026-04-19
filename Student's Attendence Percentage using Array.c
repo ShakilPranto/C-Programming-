8a0bc7a8a70742bfc84a1f01a75370cd328ba164
ShakilPranto <shakilpranto00@gmail.com>
2026-04-19 22:15:13 +0600

    Create Student's Attendence Percentage using Array.c

---
+++ Student's Attendence Percentage using Array.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int present = 0, absent = 0;
+    float percentage;
+    
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+    
+    int attendance[n];
+    
+    printf("Enter attendance (1 for Present, 0 for Absent):\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("Student %d: ", i + 1);
+        scanf("%d", &attendance[i]);
+        
+        if(attendance[i] == 1)
+        {
+            present++;
+        }
+        else if(attendance[i] == 0)
+        {
+            absent++;
+        }
+        else
+        {
+            printf("Invalid input! Use 1 or 0.\n");
+            i--;
+        }
+    }
+    percentage = (float)present / n * 100;
+    
+    printf("Total Present Students: %d\n", present);
+    printf("Total Absent Students: %d\n", absent);
+    printf("Attendance Percentage: %.2f%%\n", percentage);
+    
+    return 0;
+}
